6ff6b876c37a9f8ebfbe2cd60179477e53da9fe2
Ksenija Gogic <ksenijagogic@gmail.com>
2015-05-23 14:10:29 -0400

    Ready to mince my code up and I'm terrified.

--- scripts/main.js
+++ scripts/main.js
@@ -14,23 +14,54 @@ hundredDays.api = function(yourUniqueTag, accessTOKEN){
 		dataType: 'jsonp',
 		success: function(resp){
 			// When function is run, the API results are returned here
-			hundredDays.img(resp)
+			console.log(resp)
+			hundredDays.counter(resp)
+			hundredDays.img(resp);
+			hundredDays.likes(resp);
+			hundredDays.captions(resp);
+			hundredDays.url(resp)
 		}
 	}) // End of AJAX 
 } // End of hundredDays.api method
+
+hundredDays.counter = function(count){
+	var numb = count.data.length;
+	console.log(numb)
+	// var $countdown = $('<p class="counter">').text(numb);
+	$('.counter').text(numb + " of 100 days down!")
+	// var $counter = $('section.gallery').append($countdown);
+	// $('.counter').before('ul.gallery')
+};
 
 hundredDays.img = function(imgData){
 	var items = imgData.data;
 	$.each(items, function(key, value) {
 		var thumb = value.images.thumbnail.url;
-		var standard = value.images.standard_resolution.url
-		console.log(standard);
+		var standard = value.images.standard_resolution.url;
+		var hyperlink = value.link;
+		var likes = value.likes.count;
+		var caption = value.caption.text;
+		// console.log(standard);
 		// var $thumb = $('<li>').attr('src', thumb);
 		var $image = $('<img>').attr('src', standard);
-		var $fullSize = $('<li class="insta">').append($image);
+		var $hearts = $('<p class="likes">').html('<i class="fa fa-heart-o">').text(likes);
+		// var $heartIcon = $('<p class="likes">').html('<i class="fa fa-heart-o">');
+		var $cap = $('<p class="captions">').text(caption);
+		var $link = $('<a>').attr('href', hyperlink).attr('target', '_blank').append($image);
+		var $fullSize = $('<li class="insta">').append($link).append($hearts).append($cap);
 		$('ul.gallery').append($fullSize);
 	})
 };
+
+hundredDays.likes = function(imgData){
+	var items = imgData.data;
+	$.each(items, function(key, value){
+		var $likeCount = $('<p class="likes">').text(likes);
+		var $hearts =$('p.likes').html('<i class="fa fa-heart-o">');
+		$('ul.gallery').append($hearts, $likeCount);
+	})
+};
+
 
 $(function(){
 	hundredDays.api('100DaysCLBC', '1484007492.1677ed0.58bc2427ef174f86b431e0dbc98e3efa');
