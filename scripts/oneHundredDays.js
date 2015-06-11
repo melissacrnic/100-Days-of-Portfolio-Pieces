fb45fe665a1c90f7faf9e097925c39df29d709f5
Melissa Crnic <melissa@melissacrnic.com>
2015-06-11 19:23:46 -0400

    update for llc

---
+++ scripts/oneHundredDays.js
@@ -0,0 +1,7 @@
+$(function(){
+	// In this file, you'll insert data specific to you
+	// Insert your project hashtag WITHIN THE QUOTES
+	// Insert your access_token WITHIN THE QUOTES
+	// More info on how to obtain your access_token is at https://github.com/KsenijaGogic/100-Days-of-Portfolio-Pieces
+	hundredDays.api('ladieslearningcode', '1041515060.1677ed0.f8a44f383cb94d049aadebefed08f502');
+});
